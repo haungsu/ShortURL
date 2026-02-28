90dd64ca40dc7bca39ba7b9dd200563d342ec888
L.K. <1513593741@qq.com>
2026-02-28 13:48:28 +0800

    初始化ShortURLPro项目，添加hello接口

---
+++ src/main/java/com/example/shorturlpro/ShortUrlProApplication.java
@@ -0,0 +1,13 @@
+package com.example.shorturlpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlProApplication.class, args);
+    }
+
+}
